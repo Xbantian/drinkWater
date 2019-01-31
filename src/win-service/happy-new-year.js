cbecb9e899b13aa95f13b4216c9f877a371e4a00
zhongyi <zhongyi.qiu@xiaobao10.com>
2019-01-31 18:54:42 +0800

    新年彩蛋

--- src/win-service/happy-new-year.js
+++ src/win-service/happy-new-year.js
@@ -2,33 +2,49 @@
 let schedule = require('node-schedule');
 
 let { model, send, init } = require('../utils/to-ding');
-let { markdown } = model;
+let { text } = model;
 
-let token = null;
-try {
-    token = require('../../config/ding-token').myTest;
-    console.log(token);
-} catch (err) {
-    console.log(err);
-}
+let { tokens } = require('../utils/get-config');
 
-init(token);
+init(tokens.myTest);
 
 async function job() {
+    text.text = {
+        content: `大家好!
+我是你们的人工智障!
+新年的钟声正在敲响
+过去一年我们
+喝水预防肾结石
+喝水找到女朋友
+喝水实现跨部门！
+感谢大家一年的陪伴！
 
+新的一年
+祝福各位水友及家人
+新年快乐，身体健康
+万事如意，阖家欢乐！
+
+Happy New Year，We are Family!
+
+`
+    }
+    text.at.isAtAll = true;
+    send(text)
 }
 
 
-
-job();
-
+// job();
 
 
 function scheduleCronstyle() {
-    schedule.scheduleJob('0 0 0 1 2 * 2019', function () {
+    schedule.scheduleJob('50 53 18 31 1 * 2019', function () {
         console.log('scheduleCronstyle:' + new Date());
         job();
     });
+    // schedule.scheduleJob('58 59 23 4 2 * 2019', function () {
+    //     console.log('scheduleCronstyle:' + new Date());
+    //     job();
+    // });
 }
 
 scheduleCronstyle();
